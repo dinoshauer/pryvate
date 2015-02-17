ee62e86583a49c6d0fb4ce2927b83327d95ffd55
Kasper Jacobsen <k@mackwerk.dk>
2015-02-17 09:18:35 +0100

    Supports /simple

--- pryvate/blueprints/simple.py
+++ pryvate/blueprints/simple.py
@@ -1,7 +1,7 @@
 """Simple blueprint."""
 import os
 
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, render_template
 
 simple = Blueprint('simple', __name__, url_prefix='/simple',
                    template_folder='templates')
@@ -11,5 +11,25 @@ simple = Blueprint('simple', __name__, url_prefix='/simple',
 def get_simple():
     """List all packages."""
     packages = os.listdir(current_app.config['BASEDIR'])
+    return render_template('simple.html', packages=packages)
 
-    return 'ok'
+
+@simple.route('/<package>', methods=['GET'])
+def get_package(package):
+    """List versions of a package."""
+    package_path = os.path.join(current_app.config['BASEDIR'],
+                                package.lower())
+    files = os.listdir(package_path)
+
+    packages = []
+    for filename in files:
+        if filename.endswith('md5'):
+            with open(os.path.join(package_path, filename), 'r') as md5_digest: 
+                item = {
+                    'name': package,
+                    'version': filename.replace('.md5', ''),
+                    'digest': md5_digest.read()
+                }
+                packages.append(item)
+    return render_template('simple_package.html', packages=packages,
+                           letter=package[:1])
