1b6e4d12d09eaaab8fb2585ae665c399c0194a64
Kasper Jacobsen <k@mackwerk.dk>
2015-02-20 21:42:36 +0100

    Remove package-caching-related functions

--- pryvate/blueprints/packages/packages.py
+++ pryvate/blueprints/packages/packages.py
@@ -1,46 +1,18 @@
 """Package blueprint."""
-import hashlib
 import os
 
 import magic
-from flask import (abort, Blueprint, current_app, make_response,
-                   render_template, redirect, request)
+from flask import Blueprint, current_app, make_response, redirect, request
 
 blueprint = Blueprint('packages', __name__, url_prefix='/packages')
 
 
-def register_package(name):
-    """Register a package."""
-    package_path = os.path.join(current_app.config['BASEDIR'], name.lower())
-    if not os.path.isdir(package_path):
-        os.mkdir(package_path)
-    return True
-
-
-def save_response(name, version, response):
-    """Save the contents of a package."""
-    egg_path = os.path.join(current_app.config['BASEDIR'], name.lower(),
-                            version.lower())
-    if not os.path.isfile(egg_path):
-        with open(egg_path, 'wb') as egg:
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-                egg.write(block)
-        with open(egg_path, 'rb') as egg:
-            md5 = hashlib.md5(egg.read())
-            with open('{}.md5'.format(egg_path), 'w') as digest:
-                digest.write(md5.hexdigest())
-    return True
-
-
-@blueprint.route('/<package_type>/<letter>/<name>/<version>',
+@blueprint.route('/<_>/<__>/<name>/<version>',
                  methods=['GET', 'HEAD'])
-def packages(package_type, letter, name, version):
+def packages(_, __, name, version):
     """Get the contents of a package."""
     filepath = os.path.join(current_app.config['BASEDIR'], name.lower(),
                             version.lower())
-    remote = request.args.get('remote')
 
     if name in current_app.config['PRIVATE_EGGS']:
         if os.path.isfile(filepath):
