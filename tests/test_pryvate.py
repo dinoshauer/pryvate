883caea25c5de9ec182db422bdc094bddc70e3b6
Kasper Jacobsen <k@mackwerk.dk>
2015-02-20 08:34:00 +0100

    initial test layout

---
+++ tests/test_pryvate.py
@@ -0,0 +1,23 @@
+"""Pryvate tests."""
+
+import os
+import unittest
+import shutil
+import tempfile
+
+import pryvate
+
+
+class PryvateTestCase(unittest.TestCase):
+
+    """Main test case for Pryvate."""
+
+    def setUp(self):
+        """Set up step for all tests."""
+        self.egg_folder = tempfile.TemporaryDirectory()
+        pryvate.app.config['BASEDIR'] = self.egg_folder.name
+        self.app = pryvate.server.app.test_client()
+
+    def tearDown(self):
+        """Tear down stop for all tests."""
+        self.egg_folder.cleanup()
