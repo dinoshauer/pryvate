1f6f352c5b7d1266e241aa51e5c2a50ea7ebb739
Kasper Jacobsen <k@mackwerk.dk>
2015-02-20 09:23:40 +0100

    Add res to tests, move a test package to the tempdir so we can test it out

--- tests/test_pryvate.py
+++ tests/test_pryvate.py
@@ -12,9 +12,21 @@ class PryvateTestCase(unittest.TestCase):
 
     """Main test case for Pryvate."""
 
+    @staticmethod
+    def _copy_egg(tempdir):
+        """Copy the files from res/meep to {tempdir}/meep."""
+        base = 'tests/res/meep'
+        os.mkdir(os.path.join(tempdir, 'meep'))
+        for item in os.listdir(base):
+            shutil.copyfile(
+                os.path.join(base, item),
+                os.path.join(tempdir, 'meep', item),
+            )
+
     def setUp(self):
         """Set up step for all tests."""
         self.egg_folder = tempfile.TemporaryDirectory()
+        self._copy_egg(self.egg_folder.name)
         pryvate.server.app.config['BASEDIR'] = self.egg_folder.name
         self.app = pryvate.server.app.test_client()
 
