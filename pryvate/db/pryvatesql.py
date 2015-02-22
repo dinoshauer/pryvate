48a4fe88477f3382349d5ad8fd6065ff51c57f74
Kasper Jacobsen <k@mackwerk.dk>
2015-02-22 17:56:39 +0100

    Add SQLite connector

---
+++ pryvate/db/pryvatesql.py
@@ -0,0 +1,52 @@
+"""Pryvate database: SQLite."""
+import sqlite3
+
+
+class PryvateSQLite(object):
+
+    """Use SQLite to save egg information.
+
+    Attempts to create the ``eggs`` table if
+    it does not already exist.
+
+    Keyword Arguments:
+        name (``str``, optional): Path to the database, will be
+            interpreted as a URI if ``uri`` is ``True``
+            *Default:* ``pryvate.db``
+        uri (``bool``, optional): Pass ``True`` if you're using
+            a URI instead of a path
+            *Default:* ``False``
+    """
+
+    CREATE = 'CREATE TABLE IF NOT EXISTS eggs (name TEXT);'
+    GET_ALL = 'SELECT name FROM eggs;'
+    NEW_EGG = 'INSERT INTO eggs (name) VALUES (?);'
+
+    def __init__(self, name='pryvate.db', uri=False):
+        """Initialize a new database connection."""
+        self.connection = sqlite3.connect(name, uri=uri)
+        self.connection.execute(self.CREATE)
+        self.connection.commit()
+        self.connection.row_factory = sqlite3.Row
+
+    def get_eggs(self):
+        """Get available private eggs.
+
+        Returns:
+            ``list`` of ``str``
+        """
+        rows = self.connection.execute(self.GET_ALL)
+        return [item['name'] for item in rows]
+
+    def new_egg(self, name):
+        """Add new egg to list.
+
+        Arguments:
+            name (``str``): The name of the egg to add
+
+        Returns:
+            ``bool``
+        """
+        row_count = self.connection.execute(self.NEW_EGG, (name, )).rowcount
+        self.connection.commit()
+        return bool(row_count)
---
+++ tests/test_pryvatesql.py
@@ -0,0 +1,31 @@
+"""PryvateSQL tests."""
+# pylint: disable=too-many-public-methods
+
+import unittest
+
+from pryvate.db import PryvateSQLite
+
+
+class PryvateSQLiteTestCase(unittest.TestCase):
+
+    """Seperate test case for the PryvateSQLite class."""
+
+    def setUp(self):
+        """Set up step for all tests."""
+        self.pryvate_db = PryvateSQLite(':memory:')
+
+    def test_get_eggs(self):
+        """Test getting all eggs."""
+        expected = []
+        result = self.pryvate_db.get_eggs()
+        assert result == expected
+
+    def test_new_egg(self):
+        """Test creating a new egg."""
+        expected = True
+        result = self.pryvate_db.new_egg('Foo')
+        assert result == expected
+        expected = {'name': 'Foo'}
+        result = self.pryvate_db.connection.execute(self.pryvate_db.GET_ALL)
+        for item in result:
+            assert dict(item) == expected
